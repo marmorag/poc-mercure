762ae413885783637a6975b589cf19f14b7d0eb4
marmorag <g.marmo@hotmail.fr>
2019-12-12 17:26:36 +0100

    config + other

---
+++ src/Entity/Track.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *     mercure="true"
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\TrackRepository")
+ */
+class Track
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $burn;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getBurn(): ?int
+    {
+        return $this->burn;
+    }
+
+    public function setBurn(int $burn): self
+    {
+        $this->burn = $burn;
+
+        return $this;
+    }
+}
